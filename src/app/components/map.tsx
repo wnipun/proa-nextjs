eee170adbd77085f735c17e9904cbbab64cb5f38
Nipun Perera <nipun5perera@gmail.com>
2025-05-19 20:28:39 +1000

    Add google maps to homepage

--- src/app/components/map.tsx
+++ src/app/components/map.tsx
@@ -1,3 +1,21 @@
+"use client"
+
+import { APIProvider, Map as GoogleMap } from "@vis.gl/react-google-maps"
+
 export default function Map() {
-  return <div className="bg-amber-50 rounded-2xl h-[100%]">map box</div>
+  const apiKey = process.env.GOOGLE_MAPS_API_KEY || ""
+
+  return (
+    <div className="bg-amber-50 rounded-2xl h-[100%] overflow-hidden">
+      <APIProvider apiKey={apiKey}>
+        <GoogleMap
+          style={{ width: "100%", height: "80vh" }}
+          defaultCenter={{ lat: 22.54992, lng: 0 }}
+          defaultZoom={3}
+          gestureHandling={"greedy"}
+          disableDefaultUI={true}
+        />
+      </APIProvider>
+    </div>
+  )
 }
