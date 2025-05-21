973fc7238aab87d8260933f23e8fc3e0a5e87f0f
Nipun Perera <nipun5perera@gmail.com>
2025-05-21 22:28:46 +1000

    Format timestamp in WeatherStationDetailBlock component

--- src/app/components/weather-station-detail-block.tsx
+++ src/app/components/weather-station-detail-block.tsx
@@ -1,5 +1,6 @@
 import { RootState } from "../../../stores/store"
 import { useSelector } from "react-redux"
+import dayjs from "dayjs"
 
 export default function WeatherStationDetailBlock({
   title,
@@ -19,14 +20,22 @@ export default function WeatherStationDetailBlock({
     long_name: "",
   }
 
+  // Format timestamp
+  const formattedTimestamp =
+    title === "timestamp" ? dayjs(value).format("YYYY-MM-DD HH:mm:ss") : value
+
   return (
     <dl className="flex items-center gap-[0.25rem]">
       {long_name && <dt className="font-bold capitalize">{long_name}:</dt>}
       {!long_name && <dt className="font-bold capitalize">{title}:</dt>}
 
-      {unit && title !== 'timestamp' && <dd>{value} {unit}</dd>}
-      {!unit && title !== 'timestamp' && <dd>{value}</dd>}
-      {title === 'timestamp' && <dd>{value}</dd>}
+      {unit && title !== "timestamp" && (
+        <dd>
+          {value} {unit}
+        </dd>
+      )}
+      {!unit && title !== "timestamp" && <dd>{value}</dd>}
+      {title === "timestamp" && <dd>{formattedTimestamp}</dd>}
     </dl>
   )
 }
